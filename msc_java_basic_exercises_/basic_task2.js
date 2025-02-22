ed48a58596908c2e18ea318621180293d65cd1ce
erichestella <erichestella05@gmail.com>
2025-02-23 00:54:16 +0800

    fixed some errors in the code

--- msc_java_basic_exercises_/basic_task2.js
+++ msc_java_basic_exercises_/basic_task2.js
@@ -1,5 +1,6 @@
 // Calculator prog
 
+
 let display = document.getElementById('output');
 let first_input = "";
 let operator = "";
@@ -9,12 +10,31 @@ let second_input = "";
 document.querySelectorAll('.button').forEach(function(button){
     button.addEventListener('click', function(){
         const value = button.getAttribute('data-value');
-        if (value === "0" || value === "" ) {
-            return display.value = "Error";
-        } else if (operator === '' ) {
-            first_input += value;
-            display.value = first_input + operator;
+
+        // catches error 
+        if (value === '') {
+            display.value = "Error";
+            return;
+
+        }
+
+        // checks if the first entered number is 0
+        if (operator === ''){
+            if(first_input === '0' && value === '0') {
+                return;
+            } else if (first_input === '0' && value !== '0')
+                { 
+                    first_input = value;
+            } else {
+                // if the entered num is not 0 this func work
+                first_input += value;
+            }
+            // display the first int
+            display.value = first_input;
         } else {
+
+            // if there's an operator this func catches 2nd int
+
             second_input += value;
             display.value = second_input;
         }
@@ -24,10 +44,14 @@ document.querySelectorAll('.button').forEach(function(button){
 document.querySelectorAll('.operator_pin').forEach(function(button){
     button.addEventListener("click", function(){
         const value = button.getAttribute('data-value');
-
+        
+        // refresh the calculator 
         if (value === 'C'){
             window.location.reload();
+
         } else if (value === "=") {
+
+            // func for solving the equations
             if (first_input && operator && second_input) {
                 first_input = eval(first_input + operator + second_input).toString();
 
@@ -38,11 +62,15 @@ document.querySelectorAll('.operator_pin').forEach(function(button){
                 return display = "Error";
             }
         } else {
-            if (first_input !== "") { // Set operator only if there's a first input
+            if (first_input !== "") { 
+                
+                // Set operator only if there's a first input
                 operator = value;
                 display.value = operator;
+            } else {
+                // this func work if there's no 1st input and just operator
+                return;
             }
         }
     });
 });
-
